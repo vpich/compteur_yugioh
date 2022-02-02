f510f8aa0808d18919e5996c05d167e3168ea4c9
Virginie PICH <red_iv@hotmail.fr>
2022-02-02 13:11:59 +0100

    Refactorisation pour tous les calculs de points de vie des deux joueurs.

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-//
-//Ci-dessous se trouvent toutes les fonctions relatives à l'audio
-//
+//Fonctions relatives à l'audio
 
 const dropLpSound = new Audio("./sounds/drop_lp.mp3");
 dropLpSound.loop = true;
@@ -10,9 +8,8 @@ function playAudio(audioPath) {
     audioTitle.play();
 };
 
-//
-//Ci-dessous se trouve le code relatifs à l'activation/désactivation des boutons de calculs
-//
+
+//Activation/désactivation des boutons de calculs
 
 const player1_BtnPlus = document.querySelector('#article_player1 button.btn.btn-success');
 const player1_BtnMoins = document.querySelector('#article_player1 button.btn.btn-danger');
@@ -36,9 +33,8 @@ function activateButtons(buttons) {
     }
 };
 
-//
-//Ci-dessous se trouve le code pour la saisie des noms des joueurs
-//
+
+//Saisie des noms des joueurs
 
 let player1Name = document.getElementById('player1');
 let player1_inputName = document.querySelector("#player1_name input");
@@ -60,299 +56,163 @@ function displayPlayersName(inputName, name, inputNameParent) {
 displayPlayersName(player1_inputName, player1Name, player1_inputNameParent);
 displayPlayersName(player2_inputName, player2Name, player2_inputNameParent);
 
-//
-//Ci-dessous, le bloc pour le calcul des life-points du joueur 1
-//
+
+//Récupération des inputs des points de vie
 
 let player1_LifePoints = document.querySelector('#article_player1 h2');
 let player1_inputCalculator = document.querySelector('#article_player1 .calculateur input');
 let player1_newInputCalculator;
 
-const player1_historiqueCalcul = document.getElementById("player1_historiqueCalcul");
-
-player1_inputCalculator.addEventListener('input', function(e) {
-    if (isNaN(e.target.value) || e.target.value == 0) {
-        alert("Veuillez saisir un nombre entier.");
-        return e.target.value = "";
-    } else {
-    player1_newInputCalculator = e.target.value;
-    }
-});
-
-/*
-function newInputCalculatorValue(inputCalculator, newInputCalculator) {
-    inputCalculator.addEventListener('input', function(e) {
-        if (isNaN(e.target.value) || e.target.value == 0) {
-            alert("Veuillez saisir un nombre entier.");
-            return e.target.value = "";
-        } else {
-        newInputCalculator = e.target.value;
-        }
-    });
-};
-
-newInputCalculatorValue(player1_inputCalculator, player1_newInputCalculator);
-
-function plusCalcul(buttonPlus, lifePoints, inputCalculator, historiqueCalcul, newInputCalculator) {
-    buttonPlus.addEventListener("click", function() {
-        let actualLifePoints = lifePoints.textContent;
-        let lifePointsModifier = parseInt(actualLifePoints) + parseInt(newInputCalculator);
-
-        historiqueCalcul.innerHTML += `${actualLifePoints} + ${newInputCalculator} = ${lifePointsModifier}\r`;
-
-        if (newInputCalculator == null) {
-            alert("Veuillez remplir le champ avant de valider.");
-        } else {
-            dropLpSound.play();
-            let counter = setInterval(function() {
-                lifePoints.innerText++;
-                deactivateButtons(allCalculatorButtons);
-
-                if(lifePoints.innerText == lifePointsModifier) {
-                    clearInterval(counter);
-                    activateButtons(allCalculatorButtons);
-                    dropLpSound.pause();
-                    dropLpSound.currentTime = 0;
-                    playAudio("./sounds/lp_drop_end.mp3");
-                };
-            }, 10);
-        };
-        newInputCalculator = null;
-        inputCalculator.value = "";
-    });
-};
-
-plusCalcul(player1_BtnPlus, player1_LifePoints, player1_inputCalculator, player1_historiqueCalcul, player1_newInputCalculator);
-*/
-
-player1_BtnPlus.addEventListener("click", function() {
-    let player1_actualLifePoints = player1_LifePoints.textContent;
-    let player1_LifePointsModifier = parseFloat(player1_actualLifePoints) + parseFloat(player1_newInputCalculator);
-
-    player1_historiqueCalcul.innerHTML += `${player1_actualLifePoints} + ${player1_newInputCalculator} = ${player1_LifePointsModifier}\r`;
-
-    if (player1_newInputCalculator == null) {
-        alert("Veuillez remplir le champ avant de valider.");
-    } else {
-        dropLpSound.play();
-        let counter = setInterval(function() {
-          player1_LifePoints.innerText++;
-          deactivateButtons(player1_allCalculatorButtons);
-
-          if (player1_LifePoints.innerText == player1_LifePointsModifier) {
-                clearInterval(counter);
-                activateButtons(player1_allCalculatorButtons);
-                dropLpSound.pause();
-                dropLpSound.currentTime = 0;
-                playAudio("./sounds/lp_drop_end.mp3");
-          }
-        }, 10);
-
-    }
-
-
-player1_newInputCalculator = null;
-player1_inputCalculator.value = "";
-
-});
-
-
-player1_BtnMoins.addEventListener("click", function() {
-    let player1_actualLifePoints = player1_LifePoints.textContent;
-    let player1_LifePointsModifier = parseFloat(player1_actualLifePoints) - parseFloat(player1_newInputCalculator);
-
-    player1_historiqueCalcul.innerHTML += `${player1_actualLifePoints} - ${player1_newInputCalculator} = ${player1_LifePointsModifier}\r`;
-
-    if (player1_newInputCalculator == null) {
-        alert("Veuillez remplir le champ avant de valider.");
-    } else {
-        dropLpSound.play();
-        let counter = setInterval(function() {
-          player1_LifePoints.innerText--;
-          player1_BtnMoins.disabled = true;
-
-          if (player1_LifePoints.innerText == player1_LifePointsModifier || player1_LifePoints.innerText <= 0) {
-                if (player1_LifePoints.innerText <= 0) {
-                    clearInterval(counter);
-                    player1_BtnMoins.disabled = false;
-                    dropLpSound.pause();
-                    dropLpSound.currentTime = 0;
-                    playAudio("./sounds/lp_to_0.mp3");
-                } else {
-                        clearInterval(counter);
-                        player1_BtnMoins.disabled = false;
-                        dropLpSound.pause();
-                        dropLpSound.currentTime = 0;
-                        playAudio("./sounds/lp_drop_end.mp3");
-                    }
-
-          }
-
-        }, 10);
-    }
-
-player1_newInputCalculator = null;
-player1_inputCalculator.value = "";
-
-});
-
-
-player1_BtnDivide.addEventListener("click", function() {
-    let player1_actualLifePoints = player1_LifePoints.textContent;
-    let player1_LifePointsModifier = parseFloat(player1_actualLifePoints) / parseFloat(player1_newInputCalculator);
-
-    player1_historiqueCalcul.innerHTML += `${player1_actualLifePoints} / ${player1_newInputCalculator} = ${player1_LifePointsModifier}\r`;
-
-    if (player1_newInputCalculator == null) {
-        alert("Veuillez remplir le champ avant de valider.");
-    } else {
-        dropLpSound.play();
-        let counter = setInterval(function() {
-          player1_LifePoints.innerText--;
-          player1_BtnDivide.disabled = true;
-
-          if (player1_LifePoints.innerText == player1_LifePointsModifier) {
-                clearInterval(counter);
-                player1_BtnDivide.disabled = false;
-                dropLpSound.pause();
-                dropLpSound.currentTime = 0;
-                playAudio("./sounds/lp_drop_end.mp3");
-          }
-
-        }, 10);
-    }
-
-player1_newInputCalculator = null;
-player1_inputCalculator.value = "";
-
-});
-
-
-//
-// Ci-dessous se trouvent les calculateurs pour le joueur 2
-//
-
 let player2_LifePoints = document.querySelector('#article_player2 h2');
 let player2_inputCalculator = document.querySelector('#article_player2 .calculator input');
 let player2_newInputCalculator;
 
-player2_inputCalculator.addEventListener('input', function(e) {
+function inputValue(e) {
     if (isNaN(e.target.value) || e.target.value == 0) {
-        alert("Veuillez saisir un nombre entier.")
+        alert("Veuillez saisir un nombre entier.");
         return e.target.value = "";
     } else {
-    player2_newInputCalculator = e.target.value;
+        return e.target.value;
     }
+};
+
+player1_inputCalculator.addEventListener('input', function(e) {
+    player1_newInputCalculator = inputValue(e);
 });
 
-const player2_historiqueCalcul = document.getElementById("player2_historiqueCalcul");
+player2_inputCalculator.addEventListener('input', function(e) {
+    player2_newInputCalculator = inputValue(e);
+});
 
 
-player2_BtnPlus.addEventListener("click", function() {
-    let player2_actualLifePoints = player2_LifePoints.textContent;
-    let player2_LifePointsModifier = parseFloat(player2_actualLifePoints) + parseFloat(player2_newInputCalculator);
+// Fonction qui effectuent les calculs des points de vie et modifient le compteur
 
-    player2_historiqueCalcul.innerHTML += `${player2_actualLifePoints} + ${player2_newInputCalculator} = ${player2_LifePointsModifier}\r`;
+function lpDropEnd(allCalculatorButtons) {
+    activateButtons(allCalculatorButtons);
+    dropLpSound.pause();
+    dropLpSound.currentTime = 0;
+    playAudio("./sounds/lp_drop_end.mp3");
+};
 
-    if (player2_newInputCalculator == null) {
+function plusButtonCalcul(lifePoints, newInputCalculator, historiqueCalcul, allCalculatorButtons) {
+    let actualLifePoints = lifePoints.textContent;
+    let lifePointsModifier = parseInt(actualLifePoints) + parseInt(newInputCalculator);
+    historiqueCalcul.innerHTML += `${actualLifePoints} + ${newInputCalculator} = ${lifePointsModifier}\r`;
+
+    if (newInputCalculator == null) {
         alert("Veuillez remplir le champ avant de valider.");
     } else {
         dropLpSound.play();
         let counter = setInterval(function() {
-          player2_LifePoints.innerText++;
-          player2_BtnPlus.disabled = true;
+          lifePoints.innerText++;
+          deactivateButtons(allCalculatorButtons);
 
-          if (player2_LifePoints.innerText == player2_LifePointsModifier) {
+          if (lifePoints.innerText == lifePointsModifier) {
                 clearInterval(counter);
-                player2_BtnPlus.disabled = false;
-                dropLpSound.pause();
-                dropLpSound.currentTime = 0;
-                playAudio("./sounds/lp_drop_end.mp3");
+                lpDropEnd(allCalculatorButtons);
           }
         }, 10);
-
     }
+};
 
+function moinsButtonCalcul(lifePoints, newInputCalculator, historiqueCalcul, allCalculatorButtons) {
+    let actualLifePoints = lifePoints.textContent;
+    let lifePointsModifier = parseInt(actualLifePoints) - parseInt(newInputCalculator);
+    historiqueCalcul.innerHTML += `${actualLifePoints} - ${newInputCalculator} = ${lifePointsModifier}\r`;
 
-player2_newInputCalculator = null;
-player2_inputCalculator.value = "";
-
-});
-
-
-player2_BtnMoins.addEventListener("click", function() {
-    let player2_actualLifePoints = player2_LifePoints.textContent;
-    let player2_LifePointsModifier = parseFloat(player2_actualLifePoints) - parseFloat(player2_newInputCalculator);
-
-    player2_historiqueCalcul.innerHTML += `${player2_actualLifePoints} - ${player2_newInputCalculator} = ${player2_LifePointsModifier}\r`;
-
-    if (player2_newInputCalculator == null) {
+    if (newInputCalculator == null) {
         alert("Veuillez remplir le champ avant de valider.");
     } else {
         dropLpSound.play();
         let counter = setInterval(function() {
-          player2_LifePoints.innerText--;
-          player2_BtnMoins.disabled = true;
+          lifePoints.innerText--;
+          deactivateButtons(allCalculatorButtons);
 
-          if (player2_LifePoints.innerText == player2_LifePointsModifier || player2_LifePoints.innerText <= 0) {
-                if (player2_LifePoints.innerText <= 0) {
+          if (lifePoints.innerText == lifePointsModifier || lifePoints.innerText <= 0) {
+                if (lifePoints.innerText <= 0) {
                     clearInterval(counter);
-                    player2_BtnMoins.disabled = false;
+                    activateButtons(allCalculatorButtons);
                     dropLpSound.pause();
                     dropLpSound.currentTime = 0;
                     playAudio("./sounds/lp_to_0.mp3");
                 } else {
                         clearInterval(counter);
-                        player2_BtnMoins.disabled = false;
-                        dropLpSound.pause();
-                        dropLpSound.currentTime = 0;
-                        playAudio("./sounds/lp_drop_end.mp3");
+                        lpDropEnd(allCalculatorButtons);
                     }
-
           }
-
         }, 10);
     }
+};
 
-player2_newInputCalculator = null;
-player2_inputCalculator.value = "";
+function divideButtonCalcul(lifePoints, newInputCalculator, historiqueCalcul, allCalculatorButtons) {
+    let actualLifePoints = lifePoints.textContent;
+    let lifePointsModifier = parseInt(actualLifePoints) / parseInt(newInputCalculator);
+    historiqueCalcul.innerHTML += `${actualLifePoints} / ${newInputCalculator} = ${lifePointsModifier}\r`;
 
-});
-
-
-player2_BtnDivide.addEventListener("click", function() {
-    let player2_actualLifePoints = player2_LifePoints.textContent;
-    let player2_LifePointsModifier = parseFloat(player2_actualLifePoints) / parseFloat(player2_newInputCalculator);
-
-    player2_historiqueCalcul.innerHTML += `${player2_actualLifePoints} / ${player2_newInputCalculator} = ${player2_LifePointsModifier}\r`;
-
-    if (player2_newInputCalculator == null) {
+    if (newInputCalculator == null) {
         alert("Veuillez remplir le champ avant de valider.");
     } else {
         dropLpSound.play();
         let counter = setInterval(function() {
-          player2_LifePoints.innerText--;
-          player2_BtnDivide.disabled = true;
+          lifePoints.innerText--;
+          deactivateButtons(allCalculatorButtons);
 
-          if (player2_LifePoints.innerText == player2_LifePointsModifier) {
+          if (lifePoints.innerText == lifePointsModifier) {
                 clearInterval(counter);
-                player2_BtnDivide.disabled = false;
-                dropLpSound.pause();
-                dropLpSound.currentTime = 0;
-                playAudio("./sounds/lp_drop_end.mp3");
+                lpDropEnd(allCalculatorButtons);
           }
-
         }, 10);
     }
+};
 
-player2_newInputCalculator = null;
-player2_inputCalculator.value = "";
 
+//Calculs pour le joueur 1
+
+const player1_historiqueCalcul = document.getElementById("player1_historiqueCalcul");
+
+player1_BtnPlus.addEventListener("click", function() {
+    plusButtonCalcul(player1_LifePoints, player1_newInputCalculator, player1_historiqueCalcul, player1_allCalculatorButtons);
+    player1_newInputCalculator = null;
+    player1_inputCalculator.value = "";
 });
 
-//
+player1_BtnMoins.addEventListener("click", function() {
+    moinsButtonCalcul(player1_LifePoints, player1_newInputCalculator, player1_historiqueCalcul, player1_allCalculatorButtons);
+    player1_newInputCalculator = null;
+    player1_inputCalculator.value = "";
+});
+
+player1_BtnDivide.addEventListener("click", function() {
+    divideButtonCalcul(player1_LifePoints, player1_newInputCalculator, player1_historiqueCalcul, player1_allCalculatorButtons);
+    player1_newInputCalculator = null;
+    player1_inputCalculator.value = "";
+});
+
+
+// Calculs pour le joueur 2
+
+const player2_historiqueCalcul = document.getElementById("player2_historiqueCalcul");
+
+player2_BtnPlus.addEventListener("click", function() {
+    plusButtonCalcul(player2_LifePoints, player2_newInputCalculator, player2_historiqueCalcul, player2_allCalculatorButtons);
+    player2_newInputCalculator = null;
+    player2_inputCalculator.value = "";
+});
+
+player2_BtnMoins.addEventListener("click", function() {
+    moinsButtonCalcul(player2_LifePoints, player2_newInputCalculator, player2_historiqueCalcul, player2_allCalculatorButtons);
+    player2_newInputCalculator = null;
+    player2_inputCalculator.value = "";
+});
+
+player2_BtnDivide.addEventListener("click", function() {
+    divideButtonCalcul(player2_LifePoints, player2_newInputCalculator, player2_historiqueCalcul, player2_allCalculatorButtons);
+    player2_newInputCalculator = null;
+    player2_inputCalculator.value = "";
+});
+
+
 //Footer
-//
 
 let btn_reset = document.getElementById("reset");
 btn_reset.addEventListener("click", function() {
